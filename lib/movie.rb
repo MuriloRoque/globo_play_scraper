4aaf875eaec58d04420ee1f9f7fa7a70d9dd6d50
Murilo Roque <muriloengqui@gmail.com>
2020-04-21 07:16:04 -0300

    Created first methods inside movie class

--- lib/movie.rb
+++ lib/movie.rb
@@ -2,4 +2,14 @@ require 'nokogiri'
 require 'httparty'
 
 class Movie
+  attr_reader :link
+
+  def initialize
+    @link = 'https://www.whats-on-netflix.com/originals/'
+  end
+
+  def parsing
+    unparsed = HTTParty.get(@url)
+    parsed = Nokogiri::HTML(unparsed)
+  end
 end
