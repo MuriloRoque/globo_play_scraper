45a165e70c8ea90ac71aa6c50dc86a77c847e496
Murilo Roque <muriloengqui@gmail.com>
2020-04-21 11:29:19 -0300

    Changed from Netflix to GloboPlay

--- bin/main.rb
+++ bin/main.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 require_relative '../lib/movie.rb'
 movie = Movie.new
-puts movie.parsing
+puts movie.display
--- lib/movie.rb
+++ lib/movie.rb
@@ -5,12 +5,17 @@ class Movie
   attr_reader :link
 
   def initialize
-    @link = 'https://www.whats-on-netflix.com/originals/'
+    @link = 'https://globoplay.globo.com/busca/?q=the+big+bang+theory'
   end
 
   def parsing
     unparsed = HTTParty.get(@link)
     parsed = Nokogiri::HTML(unparsed)
     parsed
+  end
+
+  def display
+    results = parsing.css('.video-widget__title').text
+    results
   end
 end
